4d7cc996ea37d1902382e25c0bee23709e0d090c
Code like Hades <castillo.hades@gmail.comls>
2024-06-19 20:34:07 +0200

    Creating project with minimal configuration of context base path and port

---
+++ src/main/java/com/codelikehades/springbootbasicsetup/BasicSetupApp.java
@@ -0,0 +1,11 @@
+package com.codelikehades.springbootbasicsetup;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BasicSetupApp {
+    public static void main(String[] args) {
+        SpringApplication.run(BasicSetupApp.class, args);
+    }
+}
---
+++ src/test/java/com/codelikehades/springbootbasicsetup/BasicSetupAppTest.java
@@ -0,0 +1,13 @@
+package com.codelikehades.springbootbasicsetup;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BasicSetupAppTest {
+
+    @Test
+    void contextShouldLoad() {
+    }
+
+}
